b99dfd53b1952999a59c37aa8eea60c5905e9369
Кирилл <k-shved_94@mail.ru>
2013-03-24 21:29:55 +0200

    fix lab.c

--- lab1.c
+++ lab1.c
@@ -1,6 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-	printf("Hello world!");
+#include <stdio.h>
+#include <stdlib.h>
+#define name_count 256
+#define surname_count 256
+#define exam_limit 10
+struct student{
+    char name[name_count];
+    char surname[surname_count];
+    int sem_number;
+}
+struct exam{
+    int exam_number;
+    int mark;
+}
+union exams{
+    exam sem1[exam_limit];
+    exam sem2[exam_limit];
+    int sem1_count;
+    int sem2_count;
+}
+struct info{
+    student s;
+    union exams e;
 }
